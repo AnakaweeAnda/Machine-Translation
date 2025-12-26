db7ada4c6c51ebaa4e3a55d6bd9017d82a563e20
Anakawee Chujinda <xchucinda@gmail.com>
2025-12-27 02:27:41 +0700

    create dataset

--- data/clean_data.py
+++ data/clean_data.py
@@ -1,16 +1,20 @@
 import pandas as pd
 from pythainlp.tokenize import word_tokenize
-
+import re
+def clean_text(text):
+    text = re.sub(r'[^\w\s\u0E00-\u0E7F]', '', str(text))
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
 def th_segment(th_txt) :
     tokens = word_tokenize(th_txt,engine='newmm')
     return " ".join(tokens)
 
 df = pd.read_csv('data.csv')
-df['en_text'] = df['en_text']
-df['th_text'] = df['th_text'].apply(th_segment)
+df['en_text'] = df['en_text'].apply(clean_text)
+df['th_text'] = df['th_text'].apply(clean_text).apply(th_segment)
 
 df['en_text'].to_csv('en_corpus.txt',index=False,header=False)
 df['th_text'].to_csv('th_corpus.txt',index=False,header=False)
 
-
+df.to_csv('data.csv',index=False)
 
---
+++ data/utils.py
@@ -0,0 +1,39 @@
+import sentencepiece as spm
+import pandas as pd
+import tensorflow as tf
+class NMTDataset :
+    def __init__(self,en_model,th_model):
+        self.sp_en = spm.SentencePieceProcessor(model_file = en_model)
+        self.sp_th = spm.SentencePieceProcessor(model_file = th_model)
+    def encode_th(self,text) :
+        return self.sp_th.encode(text,add_bos=True,add_eos=True)
+    def encode_en(self,text) :
+        return self.sp_en.encode(text,add_bos=True,add_eos=True)
+    def decode_th(self,ids) :
+        return self.sp_th.decode(ids)
+    def decode_en(self,ids) :
+        return self.sp_en.decode(ids)
+    def create_dataset(self,csv_path,batch_size=64,max_len=150) :
+        df = pd.read_csv(csv_path)
+        df['en_text'] = df['en_text'].apply(self.encode_en)
+        df['th_text'] = df['th_text'].apply(self.encode_th)
+        dataset = tf.data.Dataset.from_tensor_slices(( 
+            tf.ragged.constant(df['en_text'].tolist()),
+            tf.ragged.constant(df['th_text'].tolist())
+            ))
+        dataset = dataset.filter(
+            lambda en,th : tf.logical_and(
+                en.shape[0] <= max_len,
+                th.shape[0] <= max_len
+            )
+        )
+        dataset = dataset.shuffle(10000).padded_batch(
+            batch_size=batch_size,
+            padded_shapes=(max_len,max_len),
+            padding_values=(0,0)
+        ).prefetch(tf.data.AUTOTUNE)
+
+        return dataset
+
+
+        
